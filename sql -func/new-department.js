7f19458d445b44b6aa22011bf1d1d82ffd58a98e
[Andrew Epermanis] <[aepermanis@gmail.com]>
2023-09-02 18:12:47 -0400

    added some sql functions and began settings up server on index.js

---
+++ sql -func/new-department.js
@@ -0,0 +1,7 @@
+
+const newDepart(db,answers) => {
+    let newId = db.query('SELECT COUNT(id) FROM department');
+    db.query('INSERT INTO department (id,label) VALUES (${}')
+}
+
+module.exports =newDepart;

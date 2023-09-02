fed0203454eb7d1a5f9c7130e86e07289830f246
[Andrew Epermanis] <[aepermanis@gmail.com]>
2023-09-02 17:02:45 -0400

    added functions to query for a view of all the specific tables

---
+++ sql -func/view-employee.js
@@ -0,0 +1,12 @@
+
+const viewEmployee(db) => {
+    db.query('SELECT * FROM employee', (err, results) => {
+        if (err){
+            console.log(err);
+        } 
+        console.log(results);
+        
+    });
+};
+
+module.exports = viewEmployee;
---
+++ sql -func/view-roles.js
@@ -0,0 +1,14 @@
+
+
+
+const viewRoles(db) => {
+    db.query('SELECT * FROM roles', (err, results) => {
+        if (err){
+            console.log(err);
+        } 
+        console.log(results);
+        
+    });
+};
+
+module.exports = viewRoles;
